67cbfaa9b680603d660225b835a087af57e66c21
jaehyunlee <jaehyunlee25@gmail.com>
2021-12-24 00:16:38 +0900

    renew

---
+++ pages/api/reservation/detailCircuitEnd.js
@@ -0,0 +1,53 @@
+import { RESPOND, ERROR, SAVE } from '../../../lib/apiCommon';
+import '../../../lib/mariaConn';
+// import { get, post } from '../../../lib/xmlHttpRequest';
+
+const QTS = {
+  // Query TemplateS
+  getGolfStatusDetail: 'getGolfStatusDetail',
+};
+const baseUrl = 'sqls/reservation/detailCircuitEnd'; // 끝에 슬래시 붙이지 마시오.
+let EXEC_STEP = '1.0.';
+
+export default async function handler(req, res) {
+  // 회원가입
+  // #1. cors 해제
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*', // for same origin policy
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type', // for application/json
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  });
+  // #2. preflight 처리
+  if (req.method === 'OPTIONS') return RESPOND(res, {});
+
+  // #3. 데이터 처리
+  // #3.1. 작업
+  try {
+    return await main(req, res);
+  } catch (e) {
+    return ERROR(res, {
+      id: 'ERR.reservation.detailCircuitEnd.3.2.2',
+      message: 'post server logic error',
+      error: e.toString(),
+      step: EXEC_STEP,
+    });
+  }
+}
+async function main(req, res) {
+  const { golf_club_id: golfClubId } = req.body;
+
+  EXEC_STEP = '3.3.';
+  const qGet = await QTS.getGolfStatusDetail.fQuery(baseUrl, {});
+  if (qGet.type === 'error')
+    return qGet.onError(res, 'getCourses.3.3.1', 'getting golf_status_detail');
+  const json = qGet.message;
+
+  SAVE(golfClubId, json);
+
+  // #3.1.3.
+  return RESPOND(res, {
+    message: '해당하는 데이터를 성공적으로 입력하였습니다.',
+    resultCode: 200,
+  });
+}
---
+++ sqls/reservation/detailCircuitEnd/getGolfStatusDetail.sql
@@ -0,0 +1,31 @@
+select 
+	gss.*,
+	gss.date,
+    gss.club_name,
+    gss.course_name,
+    sd.time_slot,
+    sd.teams,
+    sd.green_fee
+from golf_status_detail sd
+	left join (select 
+					tgs.id id, 
+                    tgs.date, 
+                    c.name club_name, 
+                    gc.name course_name
+				from (select  
+							golf_status.*
+						from  golf_status
+								left join golf_course on golf_course.id = golf_status.golf_course_id
+						where 
+							(golf_status.date, golf_status.created_at) 
+							in (select 
+									date, 
+                                    max(created_at) created_at 
+								from 
+									golf_status
+								group by date)
+						order by golf_status.date asc) tgs 
+					left join golf_club c on c.id = tgs.golf_club_id
+                    left join golf_course gc on gc.id = tgs.golf_course_id) gss
+	on sd.golf_status_id = gss.id
+where gss.id is not null;
