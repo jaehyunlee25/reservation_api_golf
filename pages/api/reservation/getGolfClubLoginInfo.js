38e01f86ce00ee67d99ecff4e70d9c472940e15c
jaehyunlee <jaehyunlee25@gmail.com>
2022-06-25 14:52:45 +0900

    renew

---
+++ pages/api/reservation/getGolfClubLoginInfo.js
@@ -0,0 +1,55 @@
+import { RESPOND, ERROR } from '../../../lib/apiCommon';
+import '../../../lib/mariaConn';
+// import { get, post } from '../../../lib/xmlHttpRequest';
+
+const QTS = {
+  // Query TemplateS
+  getClubLoginInfo: 'getClubLoginInfo',
+  // getOption: 'getOptionByProductId',
+};
+const baseUrl = 'sqls/reservation/getGolfClubLoginInfo'; // 끝에 슬래시 붙이지 마시오.
+let EXEC_STEP = '1.0.';
+
+export default async function handler(req, res) {
+  // 회원가입
+  // #1. cors 해제
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*', // for same origin policy
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type', // for application/json
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  });
+  // #2. preflight 처리
+  if (req.method === 'OPTIONS') return RESPOND(res, {});
+
+  // #3. 데이터 처리
+  // #3.1. 작업
+  try {
+    return await main(req, res);
+  } catch (e) {
+    return ERROR(res, {
+      id: 'ERR.reservation.getGolfClubs.3.2.2',
+      message: 'post server logic error',
+      error: e.toString(),
+      step: EXEC_STEP,
+    });
+  }
+}
+async function main(req, res) {
+  EXEC_STEP = '3.1.1.'; // #3.1.1. productId를 바탕으로 product 상세 정보를 얻는다.
+  const qClubs = await QTS.getClubs.fQuery(baseUrl, {});
+  if (qClubs.type === 'error')
+    return qClubs.onError(
+      res,
+      'getGolfClubLoginInfo.3.1.1',
+      'searching club login info',
+    );
+  const golfClubs = qClubs.message;
+
+  // #3.1.3.
+  return RESPOND(res, {
+    golfClubs,
+    message: '해당하는 데이터를 성공적으로 반환하였습니다.',
+    resultCode: 200,
+  });
+}
---
+++ sqls/reservation/getGolfClubLoginInfo/getClubLoginInfo.sql
@@ -0,0 +1,14 @@
+select 
+	golf_club.id,
+	golf_club.name,
+    golf_club_eng.eng_id,
+    golf_club_login_url.web,
+    golf_club_login_url.mobile,
+    golf_club_login_url.admin_id,
+    golf_club_login_url.admin_pw,
+    golf_club.created_at,
+    golf_club.updated_at
+from 
+	golf_club
+left join golf_club_eng on golf_club_eng.golf_club_id = golf_club.id
+left join golf_club_login_url on golf_club_login_url.golf_club_id = golf_club.id;
