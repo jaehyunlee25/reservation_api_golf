258aa8a9c50a323dcd3b2a9fe8ec6cf793117bd4
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-18 18:38:38 +0900

    'renew'

---
+++ sqls/reservation/newReserveSearch/delPastReserveSearch.sql
@@ -0,0 +1,6 @@
+update
+    golf_club_reserve_in_device
+set
+    isCancel = true
+where
+    golf_club_id = '${golfClubId}';    
---
+++ sqls/reservation/newReserveSearch/newReserveSearch.sql
@@ -0,0 +1,4 @@
+insert into
+    golf_club_reserve_in_device
+values
+    ${sqlValues};
