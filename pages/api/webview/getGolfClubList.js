a4860e422a706fb6bf0b7c72735d6d8b4611ce85
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-17 17:24:10 +0900

    renew

--- pages/api/webview/getGolfClubList.js
+++ pages/api/webview/getGolfClubList.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
 }
 async function main(req, res) {
   EXEC_STEP = '3.1.1.'; // #3.1.1. productId를 바탕으로 product 상세 정보를 얻는다.
-  const { keyword } = req.body;
+  let { keyword } = req.body;
   if (!keyword) keyword = "";
   const qClubs = await QTS.getGolfClubList.fQuery(baseUrl, { keyword });
   if (qClubs.type === 'error')
